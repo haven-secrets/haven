33d71c27e628401fd9c53f0af806c49688f8d677
Arpodol <24786076+arpodol@users.noreply.github.com>
2020-11-11 21:42:28 -0700

    Added setting up and tearing down of dynamodb table

--- aws/dynamodb/createTable.js
+++ aws/dynamodb/createTable.js
@@ -1,36 +1,39 @@
 import { dynamodb } from "../services.js";
 
-const tableName = "MoreSecrets";
+//const tableName = "MoreSecrets";
+const createTable = (tableName) => {
+  const params = {
+    AttributeDefinitions: [
+      {
+        AttributeName: "SecretName",
+        AttributeType: "S",
+      },
+      {
+        AttributeName: "Version",
+        AttributeType: "S",
+      },
+    ],
+    KeySchema: [
+      {
+        AttributeName: "SecretName",
+        KeyType: "HASH",
+      },
+      {
+        AttributeName: "Version",
+        KeyType: "RANGE",
+      },
+    ],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 5,
+      WriteCapacityUnits: 5,
+    },
+    TableName: tableName,
+  };
 
-const params = {
-  AttributeDefinitions: [
-    {
-      AttributeName: "SecretName",
-      AttributeType: "S",
-    },
-    {
-      AttributeName: "Version",
-      AttributeType: "S",
-    },
-  ],
-  KeySchema: [
-    {
-      AttributeName: "SecretName",
-      KeyType: "HASH",
-    },
-    {
-      AttributeName: "Version",
-      KeyType: "RANGE",
-    },
-  ],
-  ProvisionedThroughput: {
-    ReadCapacityUnits: 5,
-    WriteCapacityUnits: 5,
-  },
-  TableName: tableName,
+  dynamodb.createTable(params, function (err, data) {
+    if (err) console.log(err, err.stack);
+    else console.log(data);
+  });
 };
 
-dynamodb.createTable(params, function (err, data) {
-  if (err) console.log(err, err.stack);
-  else console.log(data);
-});
+export default createTable;
