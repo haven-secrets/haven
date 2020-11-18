e06e95c0e59b094deb0521bd28fb43d275d854fe
becco <rebecca.nguyen@queensu.ca>
2020-11-17 20:25:19 -0500

    update policies for envelope encryption

--- aws/iam/policies/createSecretWritePolicy.js
+++ aws/iam/policies/createSecretWritePolicy.js
@@ -13,21 +13,26 @@ const createSecretWritePolicy = (tableName, policyName, keyId) => {
     Statement: [
       {
         Effect: "Allow",
-        Action: ["dynamodb:PutItem"],
+        Action: "dynamodb:PutItem",
         Resource: `arn:aws:dynamodb:${region}:${accountNumber}:table/${tableName}`,
       },
       {
         Effect: "Allow",
-        Action: ["kms:Encrypt", "kms:GenerateDataKey"],
+        Action: "kms:GenerateDataKey",
         Resource: `arn:aws:kms:${region}:${accountNumber}:key/${keyId}`,
       },
+      {
+        Effect: "Allow",
+        Action: "kms:ListAliases",
+        Resource: "*",
+      }
     ],
   };
 
   const params = {
     PolicyDocument: JSON.stringify(policy),
     PolicyName: policyName,
-    Description: `Policy for writing to ${tableName} DynamoDB and encrypting the secret`,
+    Description: `Policy for writing to DynamoDB ${tableName}, listing key aliases, and generating data encryption keys`,
     Path: "/Lockit/",
   };
 
