0067be2eaa3ff6cf0fddf42be1866ed7ce838c34
becco <rebecca.nguyen@queensu.ca>
2020-11-20 10:46:28 -0500

    organize addUser command and add temporary users to a specified group

--- commands/addUser.js
+++ commands/addUser.js
@@ -1,43 +1,39 @@
 import createUser from "../aws/iam/users/createUser.js";
 import createAccessKey from "../aws/iam/users/createAccessKey.js";
 import addUserToGroup from "../aws/iam/users/addUserToGroup.js";
-// import temporaryCredentialAccessPolicy from "../aws/iam/policies/temporaryCredentialAccessPolicy.js";
-// import attachPolicyToTemporaryUser from "../aws/iam/users/attachPolicyToTemporaryUser.js";
 import { v4 as uuidv4 } from 'uuid';
 
-// TODO:
-// add temporary user to temporaryUsersGroup w/ the policy for invoking the lambda
-// add functionality for adding permanent user to multiple groups w/ their respective policies
+// TODO: discuss which hardcoded strings to remove
 
-const addUser = async (username, groupName) => {
+const createTemporaryUser = async (permanentUsername) => {
+  const temporaryUsername = "havenTemporaryUser" + uuidv4();
+  const temporaryUserData = await createUser(temporaryUsername, [{ Key: "permanentUsername", Value: permanentUsername }]);
+  const temporaryAccessKeyData = await createAccessKey(temporaryUserData.User.UserName);
+
+  const {
+    AccessKeyId: temporaryAccessKeyId,
+    SecretAccessKey: temporarySecretAccessKey,
+  } = temporaryAccessKeyData.AccessKey;
+
+  addUserToGroup("temporaryUsers", temporaryUsername);
+
+  // TODO: determine how to return the temporary username + keys and when the user will need them
+}
+
+const createPermanentUser = async (permanentUsername, groupName) => {
+  const permanentUserData = await createUser(permanentUsername);
+
+// TODO: add permanent user to multiple groups w/ their respective policies
+  if (groupName) {
+    const groupData = await addUserToGroup(groupName, permanentUserData.User.UserName);
+    console.log(groupData);
+  }
+}
+
+const addUser = async (permanentUsername, groupName) => {
   try {
-    const tags = [
-      {
-        Key: "permanentUsername",
-        Value: username,
-      }
-    ];
-
-    const temporaryUsername = "havenTemporaryUser" + uuidv4();
-    const temporaryUserData = await createUser(temporaryUsername, tags);
-    const temporaryAccessKeyData = await createAccessKey(temporaryUserData.User.UserName);
- 
-    const {
-      AccessKeyId: temporaryAccessKeyId,
-      SecretAccessKey: temporarySecretAccessKey,
-    } = temporaryAccessKeyData.AccessKey;
-
-    console.log({ temporaryUsername, temporaryAccessKeyId, temporarySecretAccessKey });
-
-    const userData = await createUser(username);
-
-    if (groupName) {
-      const groupData = await addUserToGroup(groupName, userData.User.UserName);
-      console.log(groupData);
-    }
-
-    // const policy = await temporaryCredentialAccessPolicy(temporaryAccessKeyId, policyName);
-    // await attachPolicyToTemporaryUser(username + "Temporary", policyName);
+    createTemporaryUser(permanentUsername);
+    createPermanentUser(permanentUsername, groupName);
   } catch (error) {
     console.log(error, error.stack);
   }
