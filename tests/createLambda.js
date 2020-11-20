73db4a143706ce59976e1ea877f286a6e261cebc
becco <rebecca.nguyen@queensu.ca>
2020-11-20 09:36:27 -0500

    organize code for lambda role
    Co-authored-by: Adam Isom <adamisom@users.noreply.github.com>
    Co-authored-by: arpodol <arpodol@users.noreply.github.com>
    Co-Authored-By: Dan Marino <dan-marino@users.noreply.github.com>

--- tests/createLambda.js
+++ tests/createLambda.js
@@ -5,18 +5,28 @@ so we need to create a policy for this created lambda that will be attached to t
 */
 import { lambda } from "../aws/services.js";
 import { readFileSync } from "fs";
-import createFetchCredentialsRoleForLambda from "./createFetchCredentialsRoleForLambda.js";
+// creates a specific policy for fetching user credentials
+import createFetchUserCredentialsPolicy from "./createFetchUserCredentialsPolicy.js";
+// creates generic role for lambdas
+import createLambdaRole from "./createLambdaRole.js";
+// attachs given policy to given role
+import attachRolePolicy from "./attachRolePolicy.js";
+import sleep from "../utils/sleep.js";
 
 const createFetchUserCredentialsLambda = async () => {
-  const roleData = await createFetchCredentialsRoleForLambda();
+  const { Policy } = await createFetchUserCredentialsPolicy();
+  const { Role } = await createLambdaRole();
+
+  await sleep(7000);
+  await attachRolePolicy(Policy.Arn, Role.RoleName);
 
   const params = {
     Code: {
       ZipFile: readFileSync('tests/testLambda.zip'), // TODO: how are we providing the files?
     },
-    FunctionName: 'fetchUserCredentials',
+    FunctionName: 'fetchUserCredentials2',
     Handler: 'index.handler',
-    Role: roleData.Role.Arn,
+    Role: Role.Arn,
     Runtime: "nodejs12.x", // TODO: do we need v12?
   };
 
