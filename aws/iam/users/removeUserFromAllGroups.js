785bef7c83c9c90b45a9ee24519b282f0d2b5603
becco <rebecca.nguyen@queensu.ca>
2020-11-23 13:43:25 -0500

    extract functionality from teardown user

---
+++ aws/iam/users/removeUserFromAllGroups.js
@@ -0,0 +1,9 @@
+import listGroupsForUser from "./listGroupsForUser.js";
+import removeUserFromGroup from "./removeUserFromGroup.js";
+
+const removeUserFromAllGroups = async username => {
+  const list = await listGroupsForUser(username);
+  return list.Groups.map(({ GroupName }) => removeUserFromGroup(GroupName, username));
+};
+
+export default removeUserFromAllGroups;
--- commands/teardown/teardownUser.js
+++ commands/teardown/teardownUser.js
@@ -1,50 +1,21 @@
 import { iam } from "../../aws/services.js";
+import removeUserFromAllGroups from "../../aws/iam/users/removeUserFromAllGroups.js";
 import getUserAccessKey from "../../aws/iam/users/getUserAccessKey.js";
-
-const removeUserFromGroups = async (username) => {
-  const groups = await iam.listGroupsForUser({ UserName: username }).promise();
-
-  return groups.Groups.map(group => {
-    const params = {
-      GroupName: group.GroupName,
-      UserName: username,
-    };
-
-    return iam.removeUserFromGroup(params).promise();
-  });
-};
-
-const deleteAccessKey = (accessKeyId, username) => {
-  const params = {
-    AccessKeyId: accessKeyId,
-    UserName: username,
-  };
-
-  return iam.deleteAccessKey(params).promise();
-};
-
-const deleteUser = (username) => {
-  const params = {
-    UserName: username,
-  };
-
-  return iam.deleteUser(params).promise();
-};
+import deleteUserAccessKey from "../../aws/iam/users/deleteUserAccessKey.js";
+import deleteUser from "../../aws/iam/users/deleteUser.js";
 
 const teardownUser = async (username) => {
   try {
-    const groupPromises = await removeUserFromGroups(username);
+    const groupPromises = await removeUserFromAllGroups(username);
     await Promise.all(groupPromises);
 
     const accessKeyId = await getUserAccessKey(username);
-    if (accessKeyId) await deleteAccessKey(accessKeyId, username);
+    if (accessKeyId) await deleteUserAccessKey(accessKeyId, username);
 
-    await deleteUser(username);
+    await deleteUser(username); // TODO: add a return value & confirm we need all these awaits
   } catch (error) {
     console.log(error, error.stack);
   }
 };
-
-teardownUser('foo'); // HARDCODED USERNAME
 
 export default teardownUser;
